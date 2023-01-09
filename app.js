080eddf24333a43930ef1f54d77078fb157282d4
mindspray <581796+mindspray@users.noreply.github.com>
2023-01-09 13:42:10 -0500

    Add progressive greyscale, greyscale and color switch

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 'use strict';
+
 function createDivGrid(sideLength = 50) {
-  if (sideLength > 100) {
-    sideLength = 100;
-  } else if (!sideLength || sideLength < 4) {
-    sideLength = 4;
-  }
+  if (sideLength > 100) sideLength = 100;
+  else if (!sideLength || sideLength < 4) sideLength = 4;
 
   let totalDivs = sideLength * sideLength;
   let pixels = [];
@@ -13,28 +11,39 @@ function createDivGrid(sideLength = 50) {
   }
 
   let body = document.querySelector('body');
+  let switchColor = true;
+  let switchColorBtn = document.querySelector(".switchColorBtn");
+  switchColorBtn.addEventListener("click", ()=> switchColor = !switchColor);
   
   body.addEventListener('click', (e) => {
-    if (
-      e.target.tagName === 'BODY' ||
-      e.target.className === "sideBar"
-      ) clearGrid();
+    console.log(e.target);
+    if (e.target.className === "sideBar") clearGrid();
     });
-    
     pixels.forEach((element) => {
+      let color = 100;
       element.style.setProperty('min-width', `${960 / sideLength}px`);
       element.style.setProperty('min-height', `${960 / sideLength}px`);
       element.addEventListener('mouseenter', () => {
-        element.classList.add('passed');
+        element.classList.add("passed");
+        if (switchColor) {
+          element.style.setProperty("background-color", `hsl(0, 0%, ${color-=10}%)`);
+        } else {
+          element.style.setProperty("background-color", `hsl(${random(353)}, ${random(101)}%, ${random(101)}%`);
+        }
       });
+
     let container = document.querySelector('.container');
     container.appendChild(element);
   });
 }
 
+function random(num) {
+  return Math.floor(Math.random() * num);
+}
+
 function clearGrid() {
   let passed = Array.from(document.getElementsByClassName('passed'));
-  passed.forEach((element) => element.classList.remove('passed'));
+  passed.forEach((element) => element.style.setProperty("background-color", "white"));
 }
 
 function setGridSize() {
